4ff67893aaf745daa01c580634bb401835e8c29a
Rewan-mahmoud <rewanmahmoud941@gmail.com>
2024-09-16 04:56:50 +0300

    change colors and language

---
+++ src/components/MenuArr.tsx
@@ -0,0 +1,27 @@
+export const MenuArr = [
+  {
+    menu: "الصفحة الرئيسية",
+    className: "sub-menu-down",
+    ulClassName: "sub-menu",
+    to: "/", 
+  },
+  {
+    menu: "القائمة",
+    className: "has-mega-menu",
+    to: "/our-menu-5",
+  },
+  {
+    menu: "من نحن",
+    className: "has-mega-menu",
+    to: "/about-us",
+  },
+
+  {
+    menu: "المدونة",
+    className: "has-mega-menu",
+    ulClassName: "mega-menu",
+     to: "/blog-wide-grid-sidebar"
+
+  },
+  { menu: " تواصل معنا", to: "/contact-us" },
+];
